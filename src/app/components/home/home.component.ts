4e98d58ecd2da1ebb64edc0d4d030371aa69428c
RazanAlkholy <razanalielkholy2005@gmail.com>
2025-09-29 00:19:37 +0300

    Home page..

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  featuredProducts: any[] = [];
 
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.api.getProducts().subscribe(data => {
+      this.featuredProducts = data.slice(0, 3); 
+    });
+  }
 }
